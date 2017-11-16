b30cea0ae54c8461a7d8fdca571cb10f07d24d8c
Rich <rbaird@richbairdportfolio.com>
2017-11-16 15:32:46 -0700

    fix deploy bug

--- resources/customization.rb
+++ resources/customization.rb
@@ -48,46 +48,35 @@ action :install do
     action :stop
     timeout 300
   end
+  swserver = registry_get_values(swreg(node)).select do |val|
+    val[:name] == "InstallPath"
+  end[0][:data]
 
-  ruby_block 'deploy' do
-    block do
-      $swserver = registry_get_values(swreg(node)).select do |val|
-        val[:name] == "InstallPath"
-      end[0][:data]
+  core_services = registry_get_values(csreg(node)).select do |val|
+    val[:name] == "InstallPath"
+  end[0][:data]
 
-      $core_services = registry_get_values(csreg(node)).select do |val|
-        val[:name] == "InstallPath"
-      end[0][:data]
+  mysql_path = registry_get_values("#{csreg(node)}\\Components\\MariaDB").select do |val|
+    val[:name] == 'InstallPath'
+  end[0][:data]
 
-      $mysql_path = registry_get_values("#{csreg(node)}\\Components\\MariaDB").select do |val|
-        val[:name] == 'InstallPath'
-      end[0][:data]
+  setup = load_setup(new_resource.custom_resources, swserver, core_services)
 
-      setup = load_setup(new_resource.custom_resources, $swserver, $core_services)
-
-      def deploy_customizations(dir)
-        setup = load_setup(dir, $swserver, $core_services)
+  setup["deploy"].each do |d|
+    setup = load_setup(d["package"], swserver, core_services)
+    ruby_block 'wait for ' + setup['prereq'] do
+      block do
         (1..30).each do
           p ''
         end
-        begin
-          (setup['prereq'] || []).each do |prereq|
-            p 'Waiting for the creation of ' + prereq
-            until ::File.exists?(prereq)
-              sleep 5
-            end
-          end
-        rescue NoMethodError
-          prereq = setup['prereq']
-          p 'Waiting for the creation of ' + prereq
-          until ::File.exists?(prereq)
-            sleep 5
-          end
+        p 'Waiting for the creation of ' + setup["prereq"]
+        until ::File.exists?(setup['prereq'])
+          sleep 5
         end
-        backup_and_copy(dir, $swserver, $core_services::File.join($mysql_path, 'bin'), swdata_db_user || cache_db_user, swdata_db_password || cache_db_password)
+        backup_and_copy(dir, swserver, core_services::File.join(mysql_path, 'bin'), swdata_db_user || cache_db_user, swdata_db_password || cache_db_password)
         if setup["db_schema"] && setup["db_schema"] != null
           p 'Applying Schema Changes'
-          ::Dir.chdir(::File.join($swserver, 'bin')) do
+          ::Dir.chdir(::File.join(swserver, 'bin')) do
             export_schema = ::File.join(Chef::Config['file_cache_path'], 'ex_dbschema.xml').gsub('/', "\\")
             system("start cmd /k cmd /C swdbconf.exe -import \"#{setup["db_schema"].gsub('/', '\\')}\"  -tdb swdata -cuid #{swdata_db_user || cache_db_user} -cpwd \"#{swdata_db_password || cache_db_password}\"")
             wait_for_db_schema
@@ -101,49 +90,45 @@ action :install do
             wait_for_db_schema
           end
         end
-
-        (setup['execute'] || []).each do |exec|
-          execute exec['command'] do
-            if exec['cwd']
-              cwd exec['cwd']
-            end
-            command exec['new_shell'] ? "start cmd /C cmd /C #{'"' + exec['command'] + '"'}" : exec['command']
-          end
-        end
-
-        (setup['queries'] || []).each do |db, queries|
-          queries.each do |query|
-            tmpname = ::Dir::Tmpname.make_tmpname('sql', nil)
-            tmppath = ::File.join(Chef::Config['file_cache_path'], tmpname)
-            file tmppath do
-              content <<~sql
-                use #{db};
-                #{query};
-              sql
-            end
-
-            execute query do
-              cwd ::File.join($mysql_path, 'bin')
-              command "mysql --port=5002 -u #{swdata_db_user || cache_db_user} --password=\"#{swdata_db_password || cache_db_password}\" < #{'"' + tmppath + '"'}"
-            end
-          end
-        end
-      end
-
-      setup["deploy"].each do |d|
-        deploy_customizations(::File.join(new_resource.custom_resources, d["package"]))
-      end
-      template 'restore.bat' do
-        p $swserver
-        path ::File.join(backup_folder($swserver), 'restore.bat')
-        source 'restore.bat.erb'
-        variables({
-                      :usr => swdata_db_user || cache_db_user,
-                      :pass => swdata_db_password || cache_db_password,
-                      :mysql => ::File.join($mysql_path, 'bin').gsub('/', "\\")
-                  })
       end
     end
+
+    (setup['execute'] || []).each do |exec|
+      execute exec['command'] do
+        if exec['cwd']
+          cwd exec['cwd']
+        end
+        command exec['new_shell'] ? "start cmd /C cmd /C #{'"' + exec['command'] + '"'}" : exec['command']
+      end
+    end
+
+    (setup['queries'] || []).each do |db, queries|
+      queries.each do |query|
+        tmpname = ::Dir::Tmpname.make_tmpname('sql', nil)
+        tmppath = ::File.join(Chef::Config['file_cache_path'], tmpname)
+        file tmppath do
+          content <<~sql
+            use #{db};
+            #{query};
+          sql
+        end
+
+        execute query do
+          cwd ::File.join(mysql_path, 'bin')
+          command "mysql --port=5002 -u #{swdata_db_user || cache_db_user} --password=\"#{swdata_db_password || cache_db_password}\" < #{'"' + tmppath + '"'}"
+        end
+      end
+    end
+  end
+
+  template 'restore.bat' do
+    path ::File.join(backup_folder(swserver), 'restore.bat')
+    source 'restore.bat.erb'
+    variables({
+                  :usr => swdata_db_user || cache_db_user,
+                  :pass => swdata_db_password || cache_db_password,
+                  :mysql => ::File.join(mysql_path, 'bin').gsub('/', "\\")
+              })
   end
 
   service 'ApacheServer' do
